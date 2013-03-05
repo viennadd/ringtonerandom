ae6eb732263a55ece44c10e266d4e5496b1ede38
viennadd <viennadd@gmail.com>
2013-03-06 00:15:36 +0800

    change to receiver only

--- src/com/ringtonerandom/MainActivity.java
+++ src/com/ringtonerandom/MainActivity.java
@@ -1,15 +1,84 @@
 package com.ringtonerandom;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.util.Random;
+
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.drm.DrmStore.Action;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	
+//	private Button btnOpenReceiver;
+//	private Button btnCloseReceiver;
+//	private Button btnAddRingtone;
+//	private Button btnManageRingtone;
+	
+	private TextView tvDebugMsg;
+	
+	private FileFilter filter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+//		btnOpenReceiver = (Button)findViewById(R.id.startReceiver);
+//		btnCloseReceiver = (Button)findViewById(R.id.stopReceiver);
+//		btnAddRingtone = (Button)findViewById(R.id.add);
+//		btnManageRingtone = (Button)findViewById(R.id.manage);
+//		
+//		
+//		
+//		btnOpenReceiver.setOnClickListener(this);
+//		btnCloseReceiver.setOnClickListener(this);
+//		btnAddRingtone.setOnClickListener(this);
+//		btnManageRingtone.setOnClickListener(this);
+		tvDebugMsg = (TextView)findViewById(R.id.debugMsg);
+		filter = new FileFilter() {
+			@Override
+			public boolean accept(File pathname) {
+				// TODO Auto-generated method stub
+				return pathname.getAbsolutePath().endsWith(".mp3");
+			}
+		};
+		
+		
+		// create folder
+		String strFolder = getSDPath() + "/ringtonewillrandom/";
+		File folder = new File(strFolder);
+		if (!folder.exists()) {	
+			folder.mkdirs();
+			Toast.makeText(this, "[good] make dirs", Toast.LENGTH_LONG).show();
+		}
+		
+		
+		File ringtones[] = folder.listFiles(filter);
+		
+		String msg = new String();
+		for (int i = 0; i < ringtones.length; ++i) {
+			msg += ringtones[i].getAbsolutePath() + "\n";
+		}
+		tvDebugMsg.setText(msg);
 	}
 
 	@Override
@@ -19,4 +88,26 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+//	@Override
+//	public void onClick(View v) {
+//		// TODO Auto-generated method stub
+//		if (v.equals(btnOpenReceiver)) {
+//			
+//		} else if (v.equals(btnCloseReceiver)) {
+//			
+//		} else if (v.equals(btnAddRingtone)) {
+//			
+//		} 
+//	}
+
+	
+	public String getSDPath() { 
+        File sdDir = null; 
+        boolean sdCardExist = Environment.getExternalStorageState()   
+                            .equals(android.os.Environment.MEDIA_MOUNTED); //判断sd卡是否存在 
+        if (sdCardExist) {
+        	sdDir = Environment.getExternalStorageDirectory(); //获取跟目录 
+        }
+        return sdDir.toString();
+	}
 }
---
+++ src/com/ringtonerandom/RingtoneRandomReceiver.java
@@ -0,0 +1,71 @@
+package com.ringtonerandom;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.Random;
+
+import android.content.BroadcastReceiver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.widget.Toast;
+
+public class RingtoneRandomReceiver extends BroadcastReceiver {
+
+	private FileFilter filter;
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+	
+		filter = new FileFilter() {
+			@Override
+			public boolean accept(File pathname) {
+				// TODO Auto-generated method stub
+				return pathname.getAbsolutePath().endsWith(".mp3");
+			}
+		};
+		
+		String strFolder = getSDPath() + "/ringtonewillrandom/";
+		File folder = new File(strFolder);
+
+		File ringtones[] = folder.listFiles(filter);
+
+		if (ringtones.length == 0) {
+			Toast.makeText(context, "no ringtones", Toast.LENGTH_LONG).show();
+			return;
+		}
+		
+		int n = new Random().nextInt() % ringtones.length; 
+		
+		File file = ringtones[n];
+		
+		ContentValues values = new ContentValues();  
+		values.put(MediaStore.MediaColumns.DATA, file.getAbsolutePath());  
+		values.put(MediaStore.MediaColumns.MIME_TYPE, "audio/mp3");  
+
+		values.put(MediaStore.Audio.Media.IS_RINGTONE, true);  
+		values.put(MediaStore.Audio.Media.IS_NOTIFICATION, false);  
+		values.put(MediaStore.Audio.Media.IS_ALARM, false);  
+		values.put(MediaStore.Audio.Media.IS_MUSIC, false);  
+    
+		Uri uri = MediaStore.Audio.Media.getContentUriForPath(file.getAbsolutePath());  
+		Uri newUri = context.getContentResolver().insert(uri, values);  
+		RingtoneManager.setActualDefaultRingtoneUri(context, RingtoneManager.TYPE_RINGTONE, newUri);
+	}
+
+
+	public String getSDPath() { 
+        File sdDir = null; 
+        boolean sdCardExist = Environment.getExternalStorageState()   
+                            .equals(android.os.Environment.MEDIA_MOUNTED); //判断sd卡是否存在 
+        if (sdCardExist) {
+        	sdDir = Environment.getExternalStorageDirectory(); //获取跟目录 
+        }
+        return sdDir.toString();
+	}
+}
